93380f174963dbd5dc0f722cacb3cfc08a8c4fb1
Mustafa Bitar <mustafa.bitar.dev@gmail.com>
2025-02-16 19:38:38 +0300

    Build Form, Tree and search views with chatter
    Since this is the first branch all details for this branch are in README.md file.
    Upcoming branches will have details in the description.

--- hr_grade_pay/__manifest__.py
+++ hr_grade_pay/__manifest__.py
@@ -3,6 +3,11 @@
   'author': 'CNC',
   'category': 'Human Resources/Employees',
   'version': '17.0.0.1.0',
-  'depends': ['base'],
+  'depends': ['base', 'mail'],
   'application': True,
+  'data': [
+    'security/ir.model.access.csv',
+    'views/base_menu.xml',
+    'views/hr_grade_view.xml'
+  ]
 }
