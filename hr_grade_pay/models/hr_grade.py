314fcf3c65e7993ef44b8505e6e675001c40be9a
Mustafa Bitar <mustafa.bitar.dev@gmail.com>
2025-02-16 22:13:44 +0300

    Apply simple python constrains
    1- Python constrains on grade number
    2- Add digit constrains on fields in hr_grade model

--- hr_grade_pay/models/hr_grade.py
+++ hr_grade_pay/models/hr_grade.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class HrGrade(models.Model):
     _name = 'hr.grade'
@@ -9,20 +10,21 @@ class HrGrade(models.Model):
     grade_number = fields.Integer('Grade', required=True, copy=False, tracking=True)
     
     # Rates fields
-    female_rate = fields.Float(default=1, tracking=True)
-    disability_rate = fields.Float(default=1, tracking=True)  
+    female_rate = fields.Float(default=1, tracking=True, digits=(0,2))
+    disability_rate = fields.Float(default=1, tracking=True, digits=(0,2))  
     day_hours = fields.Float(
         string='Required working hours per day', 
         default=8,
         help='This will affect the final pay based on the contract for each employee',
-        tracking=True)
+        tracking=True, 
+        digits=(0,2))
           
     # Allowances fields
-    housing_allowance = fields.Float(tracking=True) 
-    education_allowance = fields.Float(tracking=True) 
-    transport_allowance = fields.Float(tracking=True)
-    medical_allowance  = fields.Float(tracking=True) 
-    children_allowance = fields.Float(tracking=True)
+    housing_allowance = fields.Float(tracking=True, digits=(0,2)) 
+    education_allowance = fields.Float(tracking=True, digits=(0,2)) 
+    transport_allowance = fields.Float(tracking=True, digits=(0,2))
+    medical_allowance  = fields.Float(tracking=True, digits=(0,2)) 
+    children_allowance = fields.Float(tracking=True, digits=(0,2))
     
     # Special fileds
     is_retirement_eligible = fields.Boolean(string='Ability to retire', tracking=True)
@@ -34,3 +36,10 @@ class HrGrade(models.Model):
         ('lead', 'Lead/Expert'),
         ('executive', 'Executive')], 
         tracking=True)
+    
+    # Logic level constrains
+    @api.constrains('grade_number')
+    def _check_positive(self):
+        for rec in self:
+            if rec.grade_number < 1:               
+                raise ValidationError('Grade Number Should Be A Positive Integer')
